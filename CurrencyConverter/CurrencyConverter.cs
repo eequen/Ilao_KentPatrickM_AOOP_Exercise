d45d2de62f21ed08f5e3a67fcdf095d87211302a
Kent Patrick Ilao <kentpatrickilao@gmail.com>
2026-02-09 09:52:50 +0800

    Add files via upload

---
+++ CurrencyConverter/CurrencyConverter.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Enter the amount in USD: ");
+double amountInUSD = double.Parse(Console.ReadLine());
+Console.Write("Enter exchange rate from USD to PHP: ");
+double exchangeRate = double.Parse(Console.ReadLine());
+double amountInPHP = amountInUSD * exchangeRate;
+
+Console.WriteLine("Amount in PHP: " + amountInPHP);
