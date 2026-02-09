8ebc1d4fd218aeb813670bf62158e581ff26a009
Kent Patrick Ilao <kentpatrickilao@gmail.com>
2026-02-09 09:50:19 +0800

    Add files via upload

---
+++ InterestCalculator/InterestCalculator.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Enter the principal amount: ");
+double principal = double.Parse(Console.ReadLine());
+Console.Write("Enter rate of interest: ");
+double rate = double.Parse(Console.ReadLine());
+Console.Write("Enter time in years: ");
+int time = int.Parse(Console.ReadLine());
+
+double simpleInterest = (principal * rate * time) / 100;
+Console.WriteLine($"Simple Interest: {simpleInterest}");
